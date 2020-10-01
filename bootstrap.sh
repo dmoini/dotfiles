12d85543419f29f874bb5ea57d8be26f8a257d2f
Donovan Moini <donovanmoini@gmail.com>
2020-10-01 16:59:04 -0700

    Updated bootstrap.sh. Still WIP for oh-my-zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,20 @@
 
 # NOTE: WORK IN PROGRESS
 
-##### Start of helper functions #####
+LCYAN="\033[0;96m"
+NC="\033[0m"
+
+####################
+# Helper functions #
+####################
+
+bootstrapEcho() {
+    echo -e "${LCYAN}Installing $1 and related files and packages...${NC}"
+}
 
 commandNotFound() {
     for cmd in "$@"; do
-        if ! which $cmd &>/dev/null; then
+        if ! which "$cmd" &>/dev/null; then
             echo -e "$cmd is not found\n"
             return 0
         fi
@@ -16,7 +25,7 @@ commandNotFound() {
 
 directoryNotFound() {
     for dir in "$@"; do
-        if ! [[ -d $dir ]]; then
+        if ! [[ -d "$dir" ]]; then
             echo -e "$dir is not found\n"
             return 0
         fi
@@ -37,76 +46,95 @@ isNotFound() {
 }
 
 isRunningBootstrapInDotfilesFolder() {
-    if [[ $PWD == *dotfiles ]]; then
+    if [[ "$PWD" == *dotfiles ]]; then
         return 0
     else
         return 1
     fi
 }
 
-##### End of helper functions #####
+#######################
+# Bootstrap functions #
+#######################
 
-bootstrapOhMyZsh() {
-    # Install Oh My Zsh if not already installed (https://ohmyz.sh/)
-    if isNotFound -d $HOME/.oh-my-zsh; then
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    fi
+bootstrapGit() {
+    bootstrapEcho "Git"
+    
+    brew install git
 
-    cp oh-my-zsh/.zshrc $HOME
-    # TODO: move OhMyZsh files and custom plugins
+    cp git/* "$HOME"
 }
 
 bootstrapHomebrew() {
+    bootstrapEcho "Homebrew"
+
     # Install Homebrew if not already installed (https://brew.sh/)
     if isNotFound -c brew; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi
 
-    # TODO: install necessary stuff before running `brew bundle dump` (mas login)
     brew bundle install --file=homebrew/Brewfile
 }
 
-bootstrapGit() {
-    cp -r git/ $HOME
+# Must be run before bootstrapNPM
+bootstrapNode() {
+    bootstrapEcho "Node"
+
+    brew install nvm
+
+    node_version_regex="\d{1,2}\.\d{1,2}\.\d{1,2}"
+    latest_node_version=$(nvm ls-remote | tail -n1 | grep -Eo $node_version_regex)
+    nvm install "$latest_node_version"
+    nvm alias default "$latest_node_version"
 }
 
 bootstrapNPM() {
-    # TODO: check that latest Node is installed
-    # If not, save name of npm packages, upgrade node, install latest npm for new node version, reassign nvm default alias, uninstall npm packages from old node version, and uninstall old node version
-    cat npm/npm-global-packages.txt | xargs npm i -g
+    bootstrapEcho "NPM"
+
+    xargs npm i -g < npm/npm-global-packages.txt
+}
+
+bootstrapOhMyZsh() {
+    bootstrapEcho "Oh My Zsh"
+
+    # Install Oh My Zsh if not already installed (https://ohmyz.sh/)
+    if isNotFound -d "$HOME/.oh-my-zsh"; then
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
+
+    cp oh-my-zsh/.zshrc "$HOME"
+    # TODO: move OhMyZsh files and custom plugins
 }
 
 bootstrapPip() {
+    bootstrapEcho "Python"
+
+    brew install python@3.8
+
     brew link python@3.8
-    cat pip/pip-global-packages.txt | xargs pip install -U
+    xargs pip install -U < pip/pip-global-packages.txt
 }
 
 bootstrapScripts() {
-    if isNotFound -d $HOME/code/scripts; then
-        mkdir -p $HOME/code/scripts
-    fi
+    bootstrapEcho "scripts"
 
-    cp -r scripts/ $HOME/code/scripts
+    mkdir -p "$HOME/code"
+    cp -r scripts "$HOME/code"
 }
 
 bootstrapShell() {
-    cp shell/{.aliases,.exports,.functions} $HOME
-}
+    bootstrapEcho "shell files"
 
-bootstrapVSCode() {
-    VSCODE_DIR="$HOME/Library/Application Support/Code/User"
-    # TODO
+    cp shell/* "$HOME"
 }
 
 bootstrapVim() {
-    cp vim/.vimrc $HOME
-    # TODO
-}
+    bootstrapEcho "Vim"
 
-# TODO: delete testFunction and remove testFunction from bootstrapAll
-testFunction() {
-    DIR="$(cd "$(dirname "$0")" && pwd)"
-    echo $DIR
+    cp vim/.vimrc "$HOME"
+    cp -r vim/.vim "$HOME"
+
+    echo -e "To complete installation of vim plugins, type :PlugInstall in your vimrc"
 }
 
 bootstrapAll() {
@@ -115,16 +143,16 @@ bootstrapAll() {
         echo "Usage: cd /path/to/dotfiles && source bootstrap.sh"
         return 1
     fi
+    
+    bootstrapHomebrew
     # bootstrapOhMyZsh
-    # bootstrapHomebrew
     bootstrapGit
+    bootstrapNode
     bootstrapNPM
     bootstrapPip
     bootstrapScripts
     bootstrapShell
-    # bootstrapVim
-    # bootstrapVSCode
-    testFunction
+    bootstrapVim
 }
 
 bootstrapAll
